621b64a79dd74dd83f7acaac41a705c2b410327e
samsul <samsul.marif100@gmail.com>
2023-11-15 10:28:04 +0800

    add async web server arduino

--- .pio/build/esp32doit-devkit-v1/FrameworkArduino/USB.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32doit-devkit-v1/FrameworkArduino/USB.cpp.o: \
- /home/vitexamp/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.cpp \
- /home/vitexamp/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
- /home/vitexamp/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
--- .pio/build/esp32doit-devkit-v1/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32doit-devkit-v1/FrameworkArduino/cbuf.cpp.o: \
- /home/vitexamp/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- /home/vitexamp/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
